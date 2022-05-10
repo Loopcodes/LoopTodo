6fae3be6b5ec6092747981e4f302dd38f8ee3855
Paul Osuigwe <osuigwepaul.o@gmail.com>
2022-05-10 03:02:50 +0100

    first commit

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDAkKi31l_L3gs1M78AzbTWkwY3pKxp1Cg",
+    authDomain: "todo-4582c.firebaseapp.com",
+    projectId: "todo-4582c",
+    storageBucket: "todo-4582c.appspot.com",
+    messagingSenderId: "745517170222",
+    appId: "1:745517170222:web:e2736915d8f91e9c74e9bf"
+  }
 };
